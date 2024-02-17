88d5370e0407d35992d578e42415f0d307b0899b
erbilalahmad <bilalahmad876923@gmail.com>
2024-02-17 18:25:00 +0530

    url corrected

--- IMS/home/views/our_members.py
+++ IMS/home/views/our_members.py
@@ -3,4 +3,4 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def ourMembers_view(request):
-    return render(request,"our_members.html")
+    return render(request,"our-members.html")
